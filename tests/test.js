32d16a3f7996e5678f337c95da0dffadd77bb988
Oliverio Sousa <47525443+oliveriosousa@users.noreply.github.com>
2021-07-26 21:45:54 +0100

    feat: improve examples and migrate tests

--- tests/test.js
+++ tests/test.js
@@ -1,50 +1,35 @@
 'use strict'
 
 const IPFS = require('ipfs')
-
-const execa = require('execa')
 const os = require('os')
 const path = require('path')
 const { nanoid } = require('nanoid')
-const {
-  waitForOutput
-} = require('test-ipfs-example/utils')
-
-async function testCli () {
-  await Promise.all([
-    startCliNode(),
-    startCliNode()
-  ])
-}
+const { node } = require('test-util-ipfs-example');
 
 async function startCliNode () {
   const repoDir = path.join(os.tmpdir(), `repo-${nanoid()}`)
+
   const opts = {
     env: {
       ...process.env,
       IPFS_PATH: repoDir
     }
   }
+
   const ipfs = require.resolve('ipfs/src/cli.js')
 
-  await execa(ipfs, ['init'], opts)
-  await execa(ipfs, ['config', 'Addresses.Swarm', '--json', JSON.stringify([`/ip4/0.0.0.0/tcp/0`, `/ip4/127.0.0.1/tcp/0/ws`])], opts)
-  await execa(ipfs, ['config', 'Addresses.API', `/ip4/127.0.0.1/tcp/0`], opts)
-  await execa(ipfs, ['config', 'Addresses.Gateway', `/ip4/127.0.0.1/tcp/0`], opts)
-  await execa(ipfs, ['config', 'Addresses.RPC', `/ip4/127.0.0.1/tcp/0`], opts)
+  await node.execa(ipfs, ['init'], opts)
+  await node.execa(ipfs, ['config', 'Addresses.Swarm', '--json', JSON.stringify([`/ip4/0.0.0.0/tcp/0`, `/ip4/127.0.0.1/tcp/0/ws`])], opts)
+  await node.execa(ipfs, ['config', 'Addresses.API', `/ip4/127.0.0.1/tcp/0`], opts)
+  await node.execa(ipfs, ['config', 'Addresses.Gateway', `/ip4/127.0.0.1/tcp/0`], opts)
+  await node.execa(ipfs, ['config', 'Addresses.RPC', `/ip4/127.0.0.1/tcp/0`], opts)
 
-  return waitForOutput('Daemon is ready', ipfs, ['daemon'], opts)
-}
-
-async function testProgramatically () {
-  await Promise.all([
-    startProgramaticNode(),
-    startProgramaticNode()
-  ])
+  await node.waitForOutput('Daemon is ready', ipfs, ['daemon'], opts)
 }
 
 async function startProgramaticNode () {
   const repoDir = path.join(os.tmpdir(), `repo-${nanoid()}`)
+
   const node = await IPFS.create({
     repo: repoDir,
     config: {
@@ -67,10 +52,20 @@ async function startProgramaticNode () {
 
 async function runTest () {
   console.info('Testing CLI recipe')
-  await testCli()
+
+  await Promise.all([
+    startCliNode(),
+    startCliNode()
+  ])
+
   console.info('Testing Programmatic recipe')
-  await testProgramatically()
+
+  await Promise.all([
+    startProgramaticNode(),
+    startProgramaticNode()
+  ])
+
   console.info('Done!')
 }
 
-module.exports = runTest
+runTest()
